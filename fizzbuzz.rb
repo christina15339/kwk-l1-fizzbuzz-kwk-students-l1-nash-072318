72665cd50701e72dddc5a5a7ca4ee58934abadd2
christina15339 <clamberg333@gmail.com>
2018-07-24 18:31:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,6 @@ if number is divisible by 3
   puts "reply with Fizz"
   elsif the number is divisible by 5
   puts "relpy with Buzz"
-else if the number is divisible by 3 and 5
+  else if the number is divisible by 3 and 5
   puts "reply with FizzBuzz"
 end 
