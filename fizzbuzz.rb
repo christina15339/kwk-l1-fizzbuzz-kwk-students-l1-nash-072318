990318f9d49425868f66f5c6be0c8dc92482962c
christina15339 <clamberg333@gmail.com>
2018-07-24 18:52:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-if number %3==0 
+if number % 3==0 
   puts "reply with Fizz"
-  elsif number %5==20
+  elsif number % 5==20
   puts "relpy with Buzz"
-  else if number %15==0 
+  else if number % 15==0 
   puts "reply with FizzBuzz"
 end
 end 
